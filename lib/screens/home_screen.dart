931e1aa2c68b5e09cc52719a2748aee5aec262d8
Avinash174 <avinashmagar15@gmail.com>
2023-07-21 22:52:51 +0530

    new feature add

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:connector/main.dart';
 import 'package:connector/models/chat_user.dart';
 import 'package:connector/screens/profile_screen.dart';
 import 'package:connector/widgets/chat_user.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:remixicon/remixicon.dart';
@@ -16,6 +17,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<chatUser> list = [];
+  final List<chatUser> searchList = [];
+  bool _isSearching = false;
 
   @override
   void initState() {
@@ -26,84 +29,135 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat Connector'),
-        leading: const Icon(Remix.home_8_line),
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Remix.search_line,
-            ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(
-              Remix.more_2_fill,
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => ProfileScreen(
-                    user: APIs.me,
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 15,
-        ),
-        child: FloatingActionButton(
-          onPressed: () async {
-            await APIs.auth.signOut();
-            await GoogleSignIn().signOut();
-          },
-          child: const Icon(
-            Remix.chat_4_line,
-          ),
-        ),
-      ),
-      body: StreamBuilder(
-        stream: APIs.getAllUser(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-            case ConnectionState.none:
-              return const Center(child: CircularProgressIndicator());
-
-            case ConnectionState.active:
-            case ConnectionState.done:
-              final data = snapshot.data?.docs;
-
-              list =
-                  data?.map((e) => chatUser.fromJson(e.data())).toList() ?? [];
-              if (list.isNotEmpty) {
-                return ListView.builder(
-                    padding: EdgeInsets.only(
-                      top: mq.height * 0.01,
-                    ),
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: list.length,
-                    itemBuilder: (context, index) {
-                      return ChatUserCard(user: list[index]);
-                    });
-              } else {
-                return const Center(
-                  child: Text(
-                    'No Connection Found',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                );
-              }
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: WillPopScope(
+        onWillPop: () {
+          if (_isSearching) {
+            setState(() {
+              _isSearching = !_isSearching;
+            });
+            return Future.value(false);
+          } else {
+            return Future.value(true);
           }
         },
+        child: Scaffold(
+          appBar: AppBar(
+            title: _isSearching
+                ? TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Name,Email,....',
+                    ),
+                    autofocus: true,
+                    style: const TextStyle(
+                      fontSize: 17,
+                      letterSpacing: 0.5,
+                    ),
+                    onChanged: (val) {
+                      searchList.clear();
+                      for (var i in list) {
+                        if (i.name.toLowerCase().contains(val.toLowerCase()) ||
+                            i.email.toLowerCase().contains(val.toLowerCase())) {
+                          searchList.add(i);
+                        }
+                        setState(() {
+                          searchList;
+                        });
+                      }
+                    },
+                  )
+                : const Text('Chat Connector'),
+            leading: const Icon(Remix.home_8_line),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  _isSearching
+                      ? CupertinoIcons.clear_circled_solid
+                      : Remix.search_line,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isSearching = !_isSearching;
+                  });
+                },
+              ),
+              IconButton(
+                icon: const Icon(
+                  Remix.more_2_fill,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ProfileScreen(
+                        user: APIs.me,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 15,
+            ),
+            child: FloatingActionButton(
+              onPressed: () async {
+                await APIs.auth.signOut();
+                await GoogleSignIn().signOut();
+              },
+              child: const Icon(
+                Remix.chat_4_line,
+              ),
+            ),
+          ),
+          body: StreamBuilder(
+            stream: APIs.getAllUser(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                case ConnectionState.none:
+                  return const Center(child: CircularProgressIndicator());
+
+                case ConnectionState.active:
+                case ConnectionState.done:
+                  final data = snapshot.data?.docs;
+
+                  list =
+                      data?.map((e) => chatUser.fromJson(e.data())).toList() ??
+                          [];
+                  if (list.isNotEmpty) {
+                    return ListView.builder(
+                        padding: EdgeInsets.only(
+                          top: mq.height * 0.01,
+                        ),
+                        physics: const BouncingScrollPhysics(),
+                        itemCount:
+                            _isSearching ? searchList.length : list.length,
+                        itemBuilder: (context, index) {
+                          return ChatUserCard(
+                              user: _isSearching
+                                  ? searchList[index]
+                                  : list[index]);
+                        });
+                  } else {
+                    return const Center(
+                      child: Text(
+                        'No Connection Found',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    );
+                  }
+              }
+            },
+          ),
+        ),
       ),
     );
   }
